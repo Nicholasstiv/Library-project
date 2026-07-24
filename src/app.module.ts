11f7480eb248f20b64af91206ae65f5f51e85ed7
Nicholasstiv <nicholas.guerino@gmail.com>
2026-07-24 01:38:11 -0400

    Book & Author models

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksModule } from './books/books.module';
 import { ConfigModule } from '@nestjs/config';
@@ -13,7 +12,6 @@ import { DatabaseModule } from './database/database.module';
       isGlobal: true,
     }),
   ],
-  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
