53ea39d1aeb13c620176a323074e9b910d7e6db9
Nicholasstiv <nicholas.guerino@gmail.com>
2026-09-18 16:05:55 -0400

    Global auth guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppService } from './app.service';
 import { BooksModule } from './books/books.module';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { AuthorModule } from './authors/author.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { JwtAuthGuard } from './guards/Jwtauthguard.guard';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -18,6 +19,6 @@ import { AuthModule } from './auth/auth.module';
     AuthModule,
     BooksModule,
   ],
-  providers: [AppService],
+  providers: [{ provide: APP_GUARD, useClass: JwtAuthGuard }],
 })
 export class AppModule {}
